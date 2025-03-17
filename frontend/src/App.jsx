3ae10cf60b7fcf86f634d9bdedfd8605005afd53
Raunak <raunak.62021@gmail.com>
2025-03-17 16:11:03 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/footer"
+import Header from "./components/header"
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Header />
+      <h1 className="text-4xl">App</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="container bg-gray-800 text-white ">
+          <p>all</p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
