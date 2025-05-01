f5e80a133d9aa8123b6661e39e8b05c21ebcac37
Raunak <raunak.62021@gmail.com>
2025-05-01 10:18:06 +0530

    some code  cleaning

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import Signup from "./pages/Signup.jsx";
-import Login from "./pages/Login.jsx";
-import Serveys from "./pages/serveys.jsx";
-import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
-import Feature from "./pages/Feature.jsx";
 
 import {
   Route,
@@ -15,11 +8,8 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
-import Error from "./pages/Error.jsx";
 
-import Dashboard from "./pages/dashboard.jsx";
 import router from "./routes/routes.jsx";
-
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
